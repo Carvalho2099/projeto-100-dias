e93ef290b06fa656608bd90a2d7fc76331ce9ded
Felipe Dutra de Carvalho <carvalhotkd@gmail.com>
2024-02-16 08:11:02 -0400

    dia 1 - rocketseat - fundamentos reactjs

---
+++ 01-React-Rocketseat/01-Iniciando-com-react.js/01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,12 @@
+import {Post} from "./Post"
+
+export function App() {
+  return (
+    <>
+     <Post author="Felipe Carvalho" content="é isso ai"/>
+     <Post author="Lisa Rayane" content="uhuuul"/>    
+    </>
+  )
+}
+
+
