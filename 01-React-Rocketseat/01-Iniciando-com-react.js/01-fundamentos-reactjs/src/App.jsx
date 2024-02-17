8ab9ae302965365d0272cba7b65a2d781fe1bf22
Felipe Dutra de Carvalho <carvalhotkd@gmail.com>
2024-02-17 08:14:52 -0400

    dia 2 - rocketseat - fundamentos reactjs estrutura aplicacao pt1

--- 01-React-Rocketseat/01-Iniciando-com-react.js/01-fundamentos-reactjs/src/App.jsx
+++ 01-React-Rocketseat/01-Iniciando-com-react.js/01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,11 @@
 import {Post} from "./Post"
+import { Header } from "./components/Header"
+import './global.css'
 
 export function App() {
   return (
     <>
+    <Header/>
      <Post author="Felipe Carvalho" content="é isso ai"/>
      <Post author="Lisa Rayane" content="uhuuul"/>    
     </>
