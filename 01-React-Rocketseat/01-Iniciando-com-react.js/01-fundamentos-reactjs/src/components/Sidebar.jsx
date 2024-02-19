8359623654c04272684648071403ef6968878134
Felipe Dutra de Carvalho <carvalhotkd@gmail.com>
2024-02-19 14:14:57 -0400

    dia 4 - rocketseat - fundamentos reactjs estrutura aplicacao pt3

--- 01-React-Rocketseat/01-Iniciando-com-react.js/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-React-Rocketseat/01-Iniciando-com-react.js/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -8,12 +9,15 @@ export function Sidebar() {
             src="https://images.unsplash.com/photo-1701888281386-5ac0e1bb1ef4?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/Carvalho2099.png" />
+
                 <strong>Felipe Carvalho</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20}/>
                     Editar seu perfil
                 </a>
             </footer>
