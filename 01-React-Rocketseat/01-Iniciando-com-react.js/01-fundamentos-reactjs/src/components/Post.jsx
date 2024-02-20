2f494bea0f7c6bb7372f90dd6297d42b50e5ead7
Felipe Dutra de Carvalho <carvalhotkd@gmail.com>
2024-02-20 14:52:39 -0400

    dia 5 - rocketseat - fundamentos reactjs estrutura aplicacao pt4

---
+++ 01-React-Rocketseat/01-Iniciando-com-react.js/01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,20 @@
+import styles from './Post.module.css'
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/Carvalho2099.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Felipe Carvalho</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+                <time dateTime='2024-02-20'>Publicado há 1h</time>
+            </header>
+            <div className={styles.content}>
+                <p>é isso ai</p>
+            </div>
+        </article>
+    )
+}
