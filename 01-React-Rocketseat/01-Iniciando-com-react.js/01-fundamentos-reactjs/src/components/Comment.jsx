af44a5781a57beeeac71b056c392d16329760756
Felipe Dutra de Carvalho <carvalhotkd@gmail.com>
2024-02-22 14:09:15 -0400

    dia 7 - rocketseat - fundamentos reactjs estrutura aplicacao pt6

---
+++ 01-React-Rocketseat/01-Iniciando-com-react.js/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,31 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+import { Avatar } from './Avatar'
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/Carvalho2099.png"/>
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Felipe Carvalho</strong>
+                            <time dateTime='2024-02-20'>Publicado há 1h</time>
+                        </div>
+                        <button title='Deletar comentário'>
+                            <Trash size={24}/>
+                        </button>
+                    </header>
+                    <p>Parabéns!!!</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
--- 01-React-Rocketseat/01-Iniciando-com-react.js/01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-React-Rocketseat/01-Iniciando-com-react.js/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,10 +1,12 @@
+import { Avatar } from './Avatar'
+import { Comment } from './Comment'
 import styles from './Post.module.css'
 export function Post() {
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/Carvalho2099.png" />
+                    <Avatar src="https://github.com/Carvalho2099.png"/>
                     <div className={styles.authorInfo}>
                         <strong>Felipe Carvalho</strong>
                         <span>Web Developer</span>
@@ -15,6 +17,21 @@ export function Post() {
             <div className={styles.content}>
                 <p>é isso ai</p>
                 <p><a href="#">teste.teste.com</a></p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea 
+                    placeholder='Deixe um comentário.'
+                />
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+            </form>
+            <div className={styles.commentList}>
+                <Comment />
+                <Comment />
+                <Comment />
             </div>
         </article>
     )
