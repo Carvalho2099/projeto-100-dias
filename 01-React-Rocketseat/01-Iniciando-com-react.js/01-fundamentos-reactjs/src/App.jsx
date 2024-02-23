e585352c8d1f0b8c0ee0c6929a22a84737888192
Felipe Dutra de Carvalho <carvalhotkd@gmail.com>
2024-02-23 17:32:43 -0400

    dia 8 - rocketseat - fundamentos reactjs motores do react pt1

--- 01-React-Rocketseat/01-Iniciando-com-react.js/01-fundamentos-reactjs/src/App.jsx
+++ 01-React-Rocketseat/01-Iniciando-com-react.js/01-fundamentos-reactjs/src/App.jsx
@@ -4,6 +4,51 @@ import styles from './App.module.css'
 import './global.css'
 import { Sidebar } from "./components/Sidebar"
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/Carvalho2099.png',
+      name: 'Felipe Carvalho',
+      role: 'Dev Fullstack'
+    },
+    content: [
+      {type: 'paragraph', content: 'É isso ai!!'},
+      {type: 'paragraph', content: 'Acabei de subir umas paradas ai.'},
+      {type: 'link', content: 'site.com.'},
+    ],
+    publishedAt: new Date('2024-05-03 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/Carvalho2099.png',
+      name: 'Felipe Carvalho',
+      role: 'Dev Fullstack'
+    },
+    content: [
+      {type: 'paragraph', content: 'É isso ai!!'},
+      {type: 'paragraph', content: 'Acabei de subir umas paradas ai.'},
+      {type: 'link', content: 'site.com.'},
+    ],
+    publishedAt: new Date('2024-05-03 20:00:00')
+  },
+  {
+    id: 3,
+    author: {
+      avatarUrl: 'https://github.com/Carvalho2099.png',
+      name: 'Felipe Carvalho',
+      role: 'Dev Fullstack'
+    },
+    content: [
+      {type: 'paragraph', content: 'É isso ai!!'},
+      {type: 'paragraph', content: 'Acabei de subir umas paradas ai.'},
+      {type: 'link', content: 'site.com.'},
+    ],
+    publishedAt: new Date('2024-05-03 20:00:00')
+  },
+];
+
 export function App() {
   return (
     <>
@@ -11,8 +56,14 @@ export function App() {
     <div className={styles.wrapper}>
       <Sidebar />
       <main>
-        <Post author="Felipe Carvalho" content="é isso ai"/>
-        <Post author="Lisa Rayane" content="uhuuul"/>    
+        {posts.map(post => {
+          return (<Post 
+            author={post.author}
+            content={post.content}
+            publishedAt={post.publishedAt}
+          />
+          )
+        })}    
       </main>
     </div>
     </>
--- 01-React-Rocketseat/01-Iniciando-com-react.js/01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-React-Rocketseat/01-Iniciando-com-react.js/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,22 +1,37 @@
+import {format, formatDistanceToNow} from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
-export function Post() {
+export function Post({author, publishedAt, content}) {
+    const publishedDateFormat = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR,
+    })
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true
+    })
+
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://github.com/Carvalho2099.png"/>
+                    <Avatar src={author.avatarUrl}/>
                     <div className={styles.authorInfo}>
-                        <strong>Felipe Carvalho</strong>
-                        <span>Web Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
-                <time dateTime='2024-02-20'>Publicado há 1h</time>
+                <time dateTime={publishedDateFormat}>{publishedDateRelativeToNow}</time>
             </header>
             <div className={styles.content}>
-                <p>é isso ai</p>
-                <p><a href="#">teste.teste.com</a></p>
+                {content.map(line => {
+                    if(line.type === 'paragraph'){
+                        return <p>{line.content}</p>
+                    }else if (line.type === 'link'){
+                        return <p><a href="#">{line.content}</a></p>
+                    }
+                })}
             </div>
 
             <form className={styles.commentForm}>
