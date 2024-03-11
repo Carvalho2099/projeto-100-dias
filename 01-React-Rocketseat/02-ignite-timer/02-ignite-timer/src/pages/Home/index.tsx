d3c48bf1cd397300b05f77c784e4cf28a89d4390
Felipe Dutra de Carvalho <carvalhotkd@gmail.com>
2024-03-11 14:17:06 -0400

    dia 25 - rocketseat - Aprofundando em Hooks pt11

--- 01-React-Rocketseat/02-ignite-timer/02-ignite-timer/src/pages/Home/index.tsx
+++ 01-React-Rocketseat/02-ignite-timer/02-ignite-timer/src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 
 import {
   CountdownContainer,
@@ -11,17 +13,33 @@ import {
   TaskInput,
 } from './styles'
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  MinutesAmountInput: zod.number()
+  .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
+  .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
+})
+
+
+type NewCiclyFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm()
+  const { register, handleSubmit, watch, reset } = useForm<NewCiclyFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema), 
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    }
+  })
 
-  function handleCreateNewCycle(data: any) {
+  function handleCreateNewCycle(data: NewCiclyFormData) {
     console.log(data)
+    reset();
   }
-
+ 
   const task = watch('task')
   const isSubmitDisable = !task
 
-export function Home() {
   return (
     <HomeContainer>
       <form onSubmit={handleSubmit(handleCreateNewCycle)}>
