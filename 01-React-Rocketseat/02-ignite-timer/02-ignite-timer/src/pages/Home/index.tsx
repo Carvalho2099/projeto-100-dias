b4ee10e52ccb774c8a95b21c803a17eff34688c9
Felipe Dutra de Carvalho <carvalhotkd@gmail.com>
2024-03-14 16:03:02 -0400

    dia 28 - rocketseat - Aprofundando em Hooks pt14

--- 01-React-Rocketseat/02-ignite-timer/02-ignite-timer/src/pages/Home/index.tsx
+++ 01-React-Rocketseat/02-ignite-timer/02-ignite-timer/src/pages/Home/index.tsx
@@ -2,6 +2,8 @@ import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
+import { useEffect, useState } from 'react'
+import { differenceInSeconds } from 'date-fns'
 
 import {
   CountdownContainer,
@@ -12,7 +14,6 @@ import {
   StartCountdownButton,
   TaskInput,
 } from './styles'
-import { useState } from 'react'
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
@@ -28,6 +29,7 @@ interface Cycle {
   id: string
   task: string
   minutesAmount: number
+  startDate: Date
 }
 
 export function Home() {
@@ -43,6 +45,18 @@ export function Home() {
     }
   })
 
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  useEffect(() => {
+    if (activeCycle) {
+      setInterval(() => {
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate),
+        )
+      }, 1000)
+    }
+  }, [activeCycle])
+
   function handleCreateNewCycle(data: NewCiclyFormData) {
     const id = String(new Date().getTime())
 
@@ -50,6 +64,7 @@ export function Home() {
       id,
       task: data.task,
       minutesAmount: data.MinutesAmountInput,
+      startDate: new Date(),
     }
 
     setCycles(state => [...state, newCycle])
@@ -58,7 +73,6 @@ export function Home() {
     reset();
   }
 
-  const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
   const minutesAmount = Math.floor(currentSeconds / 60)
@@ -95,7 +109,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
-            {...register('minutesAmount', { valueAsNumber: true })}
+            {...register("minutesAmount", { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
